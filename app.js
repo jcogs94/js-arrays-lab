7ee22b492e1f17e1caa089c2625f0fc1eadd6b70
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-03-28 11:35:47 -0400

    ex 6 solved

--- app.js
+++ app.js
@@ -71,9 +71,9 @@ Exercise 6: Replace elements
 Complete Exercise 6 in the space below:
 */
 
+foods.splice(1, 1, 'sushi', 'cupcake');
 
-
-// console.log('Exercise 6 result:', foods);
+console.log('Exercise 6 result:', foods);
 
 /*
 Exercise 7: Using the `slice()` method
